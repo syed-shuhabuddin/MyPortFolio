7f7681e53dae7143e5f4cb3d50293f8a0d6e2d6e
syed-shuhabuddin <syedshuhabuddin786@gmail.com>
2026-04-30 10:17:22 +0530

    logo&email

--- portfolio/src/components/Contact.tsx
+++ portfolio/src/components/Contact.tsx
@@ -2,7 +2,7 @@ function Contact() {
   return (
     <section id="contact">
       <h2>Contact : 8971550174</h2>
-      <p>Email: yourmail@gmail.com</p>
+      <p>Email: syedshuhabuddin786@gmail.com</p>
     </section>
   );
 }
